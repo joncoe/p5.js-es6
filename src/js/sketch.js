a67104675614be7e0ac9045bacc4cd8a514023d7
joncoe <info@joncoe.com>
2021-05-02 14:47:35 -0400

    full shape, round 1

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,45 +1,55 @@
 import p5 from 'p5';
 import '../css/style.scss';
 
-var baseFront = {
+var base = {
   p1: {
     x: 374,
     y: 440
   },
+
   p2: {
-    x: 409,
-    y: 406
+    x : 323,
+    y: 733
   },
+
+
   p3: {
-    x : 358,
-    y: 700
-  },
-  p4: {
-    x : 524,
-    y: 1139
-  },
-  p5: {
-    x : 1109,
-    y: 953
-  },
-  p6: {
-    x : 1073,
-    y: 988
-  },
-  p7: {
     x : 488,
     y: 1174
   },
-  p8: {
-    x : 323,
-    y: 733
+
+  p4: {
+    x : 1073,
+    y: 988
   },
-  p9: {
-    x : 323,
-    y: 733
+};
+var surface = {
+  p1: {
+    x: 409,
+    y: 406
+  },
+  p2: {
+    x : 358,
+    y: 700
+  },
+  p3: {
+    x : 524,
+    y: 1139
+  },
+  p4: {
+    x : 1109,
+    y: 953
+  },
+  p5: {
+    x : 1143,
+    y: 622
+  },
+  p6: {
+    x : 845,
+    y: 338
   }
 };
-var bf = baseFront; // convenient alias
+
 
 
 
@@ -88,28 +98,30 @@ const sketch = (p) => {
     // Base Shape
     p.beginShape()
     p.stroke(255)
-    p.vertex(bf.p1.x, bf.p1.y)
-    p.vertex(bf.p2.x, bf.p2.y)
-    p.vertex(bf.p3.x, bf.p3.y)
-    p.vertex(bf.p4.x, bf.p4.y)
-    p.vertex(bf.p5.x, bf.p5.y)
-    p.vertex(bf.p6.x, bf.p6.y)
-    p.vertex(bf.p7.x, bf.p7.y)
-    p.vertex(bf.p8.x, bf.p8.y)
+    p.fill(255)
+    p.vertex(base.p1.x, base.p1.y)
+    p.vertex(base.p2.x, base.p2.y)
+    p.vertex(base.p3.x, base.p3.y)
+    p.vertex(base.p4.x, base.p4.y)
+    p.vertex(surface.p4.x, surface.p4.y)
+    p.vertex(surface.p3.x, surface.p3.y)
+    p.vertex(surface.p2.x, surface.p2.y)
+    p.vertex(surface.p1.x, surface.p1.y)
+    p.endShape(p.CLOSE)
+    
+    
+    p.beginShape()
+    p.stroke('#ffcc00')
+    p.fill('#ffcc00')
+    p.vertex(surface.p1.x, surface.p1.y)
+    p.vertex(surface.p2.x, surface.p2.y)
+    p.vertex(surface.p3.x, surface.p3.y)
+    p.vertex(surface.p4.x, surface.p4.y)
+    p.vertex(surface.p5.x, surface.p5.y)
+    p.vertex(surface.p6.x, surface.p6.y)
+    p.vertex(surface.p1.x, surface.p1.y)
     p.endShape(p.CLOSE)
 
-    // // Surface Shape
-    // p.beginShape()
-    // p.stroke(255)
-    // p.vertex(bf.p1.x, bf.p1.y)
-    // p.vertex(bf.p2.x, bf.p2.y)
-    // p.vertex(bf.p3.x, bf.p3.y)
-    // p.vertex(bf.p4.x, bf.p4.y)
-    // p.vertex(bf.p5.x, bf.p5.y)
-    // p.vertex(bf.p6.x, bf.p6.y)
-    // p.vertex(bf.p7.x, bf.p7.y)
-    // p.vertex(bf.p8.x, bf.p8.y)
-    p.endShape(p.CLOSE)
     
 
   };
